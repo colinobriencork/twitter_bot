531e518b209cde4195acb17485e979e5fc2ce08c
colinobriencork <colinobriencork@icloud.com>
2020-05-29 15:54:42 -0700

    Revert "test2"
    This reverts commit cc0cf3adb94a44e1b890d47e304841235a378f86.

--- app/__main__.py
+++ app/__main__.py
@@ -9,13 +9,13 @@ import app.app as app, datetime
 
 alarm1 = datetime.time(9, 30, 0)
 alarm2 = datetime.time(21, 0, 0)
+query_dict_count = 0
 query_dict = {v:k for v, k in enumerate(['#datascience', 
                                          '#dataviz'])}
 
-query_dict_count = 0
 if __name__ == "__main__":
     while True:
-        if query_dict_count == len(query_dict):
+        if query_dict_count == 2:
             query_dict_count = 0
         app.main(alarm1, alarm2, query_dict, query_dict_count)
         query_dict_count = query_dict_count + 1
