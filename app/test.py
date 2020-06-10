065a3353c5b0aba0575043deb603973f600f4e4b
colinobriencork <colinobriencork@icloud.com>
2020-06-09 21:51:06 -0700

    test

--- app/test.py
+++ app/test.py
@@ -5,8 +5,6 @@ Created on Fri May 15 11:43:05 2020
 
 @author: Colin
 """
+import app.actions as actions
 
-import os
-path = os.path.abspath(os.getcwd())
-list_subfolders_with_paths = [f.path for f in os.scandir(path) if f.is_dir()]
-print(list_subfolders_with_paths)
+actions.insert_un_newly_followed()
