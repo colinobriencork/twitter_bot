1b78fd370931cbddd00333ca6a9818fffb0e4022
colinobriencork <colinobriencork@icloud.com>
2020-06-10 10:48:09 -0700

    test

--- app/test.py
+++ app/test.py
@@ -5,6 +5,14 @@ Created on Fri May 15 11:43:05 2020
 
 @author: Colin
 """
-import app.actions as actions
+import app.actions as actions, datetime
 
-actions.insert_un_newly_followed()
+alarm1 = datetime.time(9, 30, 0)
+alarm2 = datetime.time(21, 0, 0)
+
+actions.follow_query_users('#datascience', 
+                           less_followers_than=3000, 
+                           less_friends_than=3000, 
+                           add_deleted_users_since=60,
+                           alarm1 = alarm1,
+                           alarm2 = alarm2)
